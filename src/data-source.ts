1437731c52118263dd180c314234282c62454b2c
valentina0612 <vbecag@correo.usbcali.edu.co>
2024-04-22 15:31:36 -0500

    Proyecto base

---
+++ src/data-source.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+import * as dotenv from "dotenv";
+import { Song } from "./entities/Song.entity";
+
+
+dotenv.config();
+
+const {DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } =
+  process.env;
+  
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  port: parseInt(DB_PORT || "5432"),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  synchronize: false,
+//logging logs sql command on the treminal
+  logging:  true,
+  entities: [Song],
+  migrations: [__dirname + "/migration/*.ts"],
+  subscribers: [],
+});
